cb69f948ec113588786cfd44f9a88d2da2653360
Glen Mark Cabiladas <gcabiladas@gmail.com>
2017-08-01 14:10:49 +0800

    Animate li tags in grid view

--- js/icon-features.js
+++ js/icon-features.js
@@ -22,18 +22,28 @@ if (addyourList) {
 function visibleAddModule() {
   document.getElementById("item-search-box").value = "";
 
+  //check if search module is visible
+  function searchBoxcheckifVisible() {
+    if (document.getElementById('item-search-box').classList.contains('visible')) {
+      document.getElementById('item-search-box').classList.remove('visible');
+       document.getElementById('item-search-box').classList.toggle('hidden');
+    }
+  }
+
   var addItems = document.getElementById('item-adder-box');
-  if (addItems.classList.contains('hidden', 'show', 'animated', 'fadeInUpUp')) {
+  if (addItems.classList.contains('hidden', 'show', 'animated', 'fadeInDown')) {
     addItems.classList.remove('hidden');
-    addItems.classList.remove('animated', 'fadeInUp');
+    addItems.classList.remove('animated', 'fadeInDown');
     addItems.classList.toggle('visible');
-    addItems.classList.toggle('animated', 'fadeInUp');
-    console.log('show');
+    addItems.classList.toggle('animated');
+      addItems.classList.toggle('fadeInDown');
+    searchBoxcheckifVisible();
+    console.log('show item-adder-box');
   } else {
     addItems.classList.add('hidden');
     addItems.classList.remove('visible');
-    addItems.classList.remove('animated', 'fadeInUp');
-    console.log('remove');
+    addItems.classList.remove('animated', 'fadeInDown');
+    console.log('remove item-adder-box');
   }
 }
 
@@ -48,24 +58,28 @@ function visibleSearchModule() {
   //check if add item module is visible
   function addBoxcheckifVisible() {
     if (document.getElementById('item-adder-box').classList.contains('visible')) {
+       document.getElementById('item-adder-box').classList.remove('visible');
        document.getElementById('item-adder-box').classList.toggle('hidden');
+       console.log('item-adder-box is hidden');
     }
+
   }
 
   document.getElementById("item-adder-box").value = "";
   var searchItems = document.getElementById('item-search-box');
-  if (searchItems.classList.contains('hidden', 'show', 'animated', 'fadeInUpUp')) {
+  if (searchItems.classList.contains('hidden', 'show', 'animated', 'fadeInUp')) {
     searchItems.classList.remove('hidden');
     searchItems.classList.remove('animated', 'fadeInUp');
     searchItems.classList.toggle('visible');
-    searchItems.classList.toggle('animated', 'fadeInUp');
+    searchItems.classList.toggle('animated');
+    searchItems.classList.toggle('fadeInUp');
     addBoxcheckifVisible();
-    console.log('show module');
+    console.log('show search module');
   } else {
     searchItems.classList.add('hidden');
     searchItems.classList.remove('visible');
     searchItems.classList.remove('animated', 'fadeInUp');
-    console.log('remove module');
+    console.log('remove search module');
   }
 }
 
@@ -77,6 +91,13 @@ gridView.addEventListener('click', listGridView, false);
 
 function listGridView(){
   var gridParent = document.getElementById('myUL');
+  var gridLi = gridParent.getElementsByTagName("LI");
+
+  for (var kk = 0; kk < gridLi.length; kk++) {
+      gridLi[kk].classList.toggle('animated');
+      gridLi[kk].classList.toggle('pulse');
+  }
+
   gridParent.classList.toggle('show-grid-view');
   console.log('show grid view');
 
